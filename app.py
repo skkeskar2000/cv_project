dc3d91c94389bacc8a91a54735870b8ddbc1b36b
skkeskar2000 <88729566+skkeskar2000@users.noreply.github.com>
2022-11-18 09:26:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -76,11 +76,11 @@ class VideoProcessor:
         
     # FPS
     
-        # self.cTime = time.time()
-        # fps = 1 / (self.cTime - self.pTime)
-        # self.pTime = self.cTime
-        # cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,
-        #         1, (255, 0, 0), 3)
+        self.cTime = time.time()
+        fps = 1 / (self.cTime - self.pTime)
+        self.pTime = self.cTime
+        cv2.putText(img, f'FPS: {int(fps)}', (40, 50), cv2.FONT_HERSHEY_COMPLEX,
+                1, (255, 0, 0), 3)
 
     # Hand Distance
 
