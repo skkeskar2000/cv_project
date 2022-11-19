a03b62f36b38e8bef9d22daf2ad314a582ef8125
skkeskar2000 <88729566+skkeskar2000@users.noreply.github.com>
2022-11-19 09:29:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ import math
 # from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 # import HandTrackingModule as htm
 
-
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
