2c5569aaba2e7cf4acbc1d61bae5ddc1eac94f9f
Saurabh Keskar <88729566+skkeskar2000@users.noreply.github.com>
2022-12-12 22:36:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,11 +12,13 @@ hands = mp_hands.Hands(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5
 )
+
 def process(image):
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = hands.process(image)
-# Draw the hand annotations on the image.
+
+    # Draw the hand annotations on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     if results.multi_hand_landmarks:
@@ -28,14 +30,20 @@ def process(image):
             mp_drawing_styles.get_default_hand_landmarks_style(),
             mp_drawing_styles.get_default_hand_connections_style())
     return cv2.flip(image, 1)
+
+
 RTC_CONFIGURATION = RTCConfiguration(
     {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
 )
+
 class VideoProcessor:
     def recv(self, frame):
         img = frame.to_ndarray(format="bgr24")
+
         img = process(img)
+
         return av.VideoFrame.from_ndarray(img, format="bgr24")
+
 webrtc_ctx = webrtc_streamer(
     key="WYH",
     mode=WebRtcMode.SENDRECV,
